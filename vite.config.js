acd298bfe0c168b5d161ac1fe8a02b7e609f9ce2
Your Name <you@example.com>
2024-10-04 17:16:13 +0700

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SpotUp/', // Change to your repository name
+  base: '/', // Change to your repository name
 });
