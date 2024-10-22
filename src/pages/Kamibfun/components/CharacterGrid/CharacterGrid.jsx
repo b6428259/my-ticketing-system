b82fb18cf8cd1a424bd0f47408a075617f2c1766
Your Name <you@example.com>
2024-10-23 00:17:15 +0700

    Refactor CSS files to apply custom font and remove unused code

--- src/pages/Kamibfun/components/CharacterGrid/CharacterGrid.jsx
+++ src/pages/Kamibfun/components/CharacterGrid/CharacterGrid.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import ImageBlock from '../../../../components/ImageBlock/ImageBlock';
-import characterData from '../../data/CharacterData';
-import './CharacterGrid.css';
+import ImageBlock from '../../../../components/ImageBlock/ImageBlock'; // Import the ImageBlock component
+import characterData from '../../data/CharacterData'; // Import the character data
+import './CharacterGrid.css'; // Import the CSS file
 
 const CharacterGrid = () => {
   return (
     <div
       id="character-section"
-      className="character-grid mt-6 mx-auto justify-items-center p-4 border-1 border-black rounded-lg font-custom"
+      className="character-grid mt-6 mx-auto justify-items-center p-4 border-1 border-black rounded-lg"
       style={{
         width: '85%',
         maxWidth: '1000px',
       }}
     >
       {characterData.map((character) => (
-        <div 
-          key={character.id} 
-          style={{ 
-            gridArea: character.style.gridArea,
-          }}
-        >
+        <div key={character.id} 
+        style={{ 
+          gridArea: character.style.gridArea,
+           }}>
           <ImageBlock
             src={character.src}
             alt={character.alt}
